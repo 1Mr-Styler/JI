3b6aa5c15fc78330f9d3ae27393bd975797de3ce
Jerry S <udensi.jerry@gmail.com>
2017-05-18 06:01:05 +0800

    Implemented Basic NN Algorithms

--- JI/ViewController.swift
+++ JI/ViewController.swift
@@ -10,7 +10,11 @@ import Cocoa
 
 class ViewController: NSViewController {
 
-    dynamic var abs = 1.0
+    dynamic var abs = 1.0 
+    dynamic var absLab = "ABS Error"
+    dynamic var useABS = true
+    dynamic var smallredarr = true
+    dynamic var arrFromABS = false
     dynamic var inp = -0.5 {
         didSet {
             N1()
@@ -41,8 +45,9 @@ class ViewController: NSViewController {
     dynamic var result : Double = 1.0
     dynamic var n1 : Double  = 0.0
     dynamic var n2 : Double = 0.0
+    dynamic var ty : String  = "ReLU"
     
-    dynamic var relu : Double = 0.0
+    dynamic var selectedFormula : Double = 0.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -73,18 +78,61 @@ class ViewController: NSViewController {
     func N2() {
         DispatchQueue.main.async {
             self.n2 = (self.n1 * self.n2w) + self.n2b
-            
-            self.ReLu()
         }
     }
     
-    func ReLu() {
+    @IBAction func ReLu(_ sender: Any) {
         DispatchQueue.main.async {
-            self.relu = max(0.0, self.n2)
-            self.result = self.abs - self.relu
+            self.selectedFormula = max(0.0, self.n2)
+            self.result = max(0.0, (self.abs - self.selectedFormula))
+            self.ty = #function
+            self.choiceOfVisuals(ABS: true, arrowFromABS: false, smallArrow: true)
+            self.absLab = "ABS Error"
         }
     }
+    
+    @IBAction func Sigmoid(_ sender: Any) {
+        DispatchQueue.main.async {
+            self.ty = #function
+            self.choiceOfVisuals(ABS: false, arrowFromABS: false, smallArrow: false)
+            
+            self.selectedFormula = 1/(1 + exp(-self.n2))
+            self.result = self.selectedFormula
+        }
+    }
+    
+    @IBAction func Linear(_ sender: Any) {
+        self.ty = #function
+        self.selectedFormula = self.linear(x: [self.n1], w: [self.n2w])
+        self.result = self.abs - self.selectedFormula
+        self.choiceOfVisuals(ABS: true, arrowFromABS: false, smallArrow: true)
+        self.absLab = "ABS Error"
+    }
+    
+    @IBAction func Binary(_ sender: Any) {
+        self.ty = #function
+        self.choiceOfVisuals(ABS: true, arrowFromABS: true, smallArrow: false)
+        
+        let bin = self.n1 * self.n2w
+        self.n2 = bin
+        self.absLab = "Threshold"
+        self.selectedFormula = bin > self.abs ? 1 : 0
+        self.result = self.selectedFormula
+    }
+    
+    func choiceOfVisuals(ABS: Bool, arrowFromABS: Bool, smallArrow: Bool) {
+        self.useABS = ABS
+        self.smallredarr = smallArrow
+        self.arrFromABS = arrowFromABS
+    }
 
+    func linear(x: [Double], w: [Double]) -> Double {
+        var liner = 0.0
+        for i in 0..<x.count {
+            liner += x[i] * w[i]
+        }
+        return liner + self.n2b
+    }
 
 }
 
